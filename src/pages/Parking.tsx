7f6f41b0324d0e8fbf2d8aa1dd8e09c065b691de
Tiago Raposo <tiagoppal@gmail.com>
2026-05-25 23:43:00 +0200

    fix rerender on parking page

--- src/pages/Parking.tsx
+++ src/pages/Parking.tsx
@@ -5,9 +5,6 @@ import { useParking } from '../hooks/useParking'
 export function Parking() {
   const { list, loading, error, search, setSearch, sortField, setSortField, sortDirection, setSortDirection, handleSearch } = useParking()
 
-  if (loading) return <div>Loading...</div>
-  if (error) return <div>Error: {error}</div>
-
   return (
     <>
       <h1>Parking</h1>
@@ -20,7 +17,11 @@ export function Parking() {
         setSortDirection={setSortDirection}
         onSearch={handleSearch}
       />
-      {list.length === 0 ? (
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>Error: {error}</div>
+      ) : list.length === 0 ? (
         <div>No parkings available</div>
       ) : (
         <ul className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-4">
